5e9b05d3d5d2b0719c65a3bb651811b2005ebd3e
jlorenzo58 <jlorenzo58@icloud.com>
2021-12-29 08:06:44 -0500

    commit

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,6 +1,10 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
+  //divide by 2 and floor
+  return (Math.floor(num/2));
 }
+
+console.log(cutInHalfAndFloor(99));
 
 // Do not edit this line;
 module.exports = cutInHalfAndFloor;
--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,15 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var counter=0;
+  for(let i = 0; i < arr.length;i++){
+    if(typeof arr[i] == "boolean"){
+      counter++;
+    }
+  }
+  return counter;
 }
+
+console.log(countOfAllBooleans([true, false, true, false, true, 7]));
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
