c232b30b4f3ad093a297cc9170a33aa58e3a381d
jlorenzo58 <jlorenzo58@icloud.com>
2021-12-29 08:25:10 -0500

    commit

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var counter=0;
+  for(let i=0;i<arr.length;i++){
+    if( (typeof arr[i] == "string") || (typeof arr[i] == "boolean")){
+      counter++;
+    }
+  }
+  return counter;
 }
+console.log(countOfAllBooleansAndStrings([8, "hello", undefined, null, false, false, false, false, false]));
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,14 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var counter =0;
+  for(let i=0;i<nums.length;i++){
+    if(nums[i]<target){
+      counter++;
+    }
+  }
+  return counter;
 }
 
+console.log(countOfAllNumbersSmallerThanTarget([9000],22));
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
