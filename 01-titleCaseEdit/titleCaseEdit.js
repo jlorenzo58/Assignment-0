a9448466efd8de9c0a6f4bca743930781f4ed5bf
jlorenzo58 <jlorenzo58@icloud.com>
2021-12-29 07:26:39 -0500

    first commit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,26 @@
 function titleCaseEdit(title) {
   // Insert code here;
+
+  //split into words to manipulate each word individually.
+  const words = title.split(" ");
+  
+  //will be returned as the updated title
+  var update="";
+
+  //loop through the words 
+  for(let i=0; i< words.length;i++){
+    //capatalize each first letter, then push the new word into the updated title
+    words[i]=words[i][0].toUpperCase()+words[i].substr(1);
+    update += words[i] + " ";
+  }
+  //to obey the "it should return a string of the same length as the input title"
+  update=update.slice(0,-1);
+
+  //return
+  return update;
 }
+
+console.log(titleCaseEdit("here comes the sun"));
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,8 @@
 function subtractFive(num) {
   // Insert code here;
+  return num-5;
 }
+console.log(subtractFive(20));
 
 // Do not edit this line;
 module.exports = subtractFive;
