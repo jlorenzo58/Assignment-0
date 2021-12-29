6a87670f6115854d77c3a08c43c95ad990c43080
jlorenzo58 <jlorenzo58@icloud.com>
2021-12-29 11:08:46 -0500

    commit

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  var min = nums[0],max=nums[0],sum = 0;
+  for(let i=0; i < nums.length; i++){
+    if(min>nums[i]){
+      min=nums[i];
+    }
+    if(max<nums[i]){
+      max=nums[i];
+    }
+  }
+  return min+max;
 }
 
+console.log(sumOfMinimumAndMaximum([5,5,5,5,5,5,5]));
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,34 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+//this array will be filled and returned.  
+let temp=[];
+//constant strings that will be values used in filling the array
+const fizz="Fizz";
+const buzz="Buzz";
+//for each element in between start and end....
+for(start, i=0; start<=end; start++,i++)
+{
+  if((start%3==0)&&(start%5==0)){  //determine if the number is both a multiple of 3 and 5, if so, fill with fizzBuzz, continute
+    temp[i] = fizz + buzz;
+    continue;
+  }
+  else if(start%3==0){   //if number is only a multiple of 3, fill index with fizz, continue
+    temp[i]=fizz;
+    continue;
+  }
+  else if(start%5==0){  //if number is only a multiple of 5, fill index with buzz, continue
+    temp[i]=buzz;
+    continue;
+  }
+  else    //else just fill the index with the number
+    temp[i]=start;
 }
+
+return temp;  //return
+
+}
+
+console.log(fizzBuzz(15,16));
 
 // Do not edit this line;
 module.exports = fizzBuzz;
